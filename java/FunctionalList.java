a075539e2a60a8b8aa41113c561962d3174a4d52
Samir Talwar <samir@noodlesandwich.com>
2011-10-24 23:00:55 +0100

    Made the FunctionalList subclasses inner classes of the abstract class.

--- java/FunctionalList.java
+++ java/FunctionalList.java
@@ -1,3 +1,5 @@
+import java.util.NoSuchElementException;
+
 public abstract class FunctionalList<T> {
     public static <T> FunctionalList<T> nil() {
         return new Nil<T>();
@@ -27,5 +29,47 @@ public abstract class FunctionalList<T> {
         return isEmpty()
                    ? FunctionalList.<U>nil()
                    : cons(mapping.apply(head()), tail().map(mapping));
+    }
+
+    public static final class Nil<T> extends FunctionalList<T> {
+        @Override
+        public boolean isEmpty() {
+            return true;
+        }
+
+        @Override
+        public T head() {
+            throw new NoSuchElementException();
+        }
+
+        @Override
+        public FunctionalList<T> tail() {
+            throw new NoSuchElementException();
+        }
+    }
+
+    public static final class Cons<T> extends FunctionalList<T> {
+        private final T head;
+        private final FunctionalList<T> tail;
+
+        public Cons(final T head, final FunctionalList<T> tail) {
+            this.head = head;
+            this.tail = tail;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return false;
+        }
+
+        @Override
+        public T head() {
+            return head;
+        }
+
+        @Override
+        public FunctionalList<T> tail() {
+            return tail;
+        }
     }
 }
