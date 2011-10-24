2512004094302520662314d61906e760b2d43abe
Samir Talwar <samir@noodlesandwich.com>
2011-10-24 23:50:33 +0100

    Implemented the functional list in Ruby.

---
+++ ruby/functional_list.rb
@@ -0,0 +1,46 @@
+module FunctionalList
+  def FunctionalList.nil
+    Nil.new
+  end
+
+  def FunctionalList.cons(head, tail)
+    Cons.new head, tail
+  end
+
+  def FunctionalList.of(*items)
+    fromArray items, 0, items.length
+  end
+
+  def map(&mapping)
+    isEmpty ? FunctionalList.nil : cons(mapping.call(head), tail.map(mapping))
+  end
+
+private
+
+  def FunctionalList.fromArray(items, start, finish)
+    start == finish ? FunctionalList.nil : cons(items[start], fromArray(items, start + 1, finish))
+  end
+end
+
+class Nil
+  include FunctionalList
+
+  def isEmpty
+    true
+  end
+end
+
+class Cons
+  include FunctionalList
+
+  attr_reader :head, :tail
+
+  def initialize(head, tail)
+    @head = head
+    @tail = tail
+  end
+
+  def isEmpty
+    false
+  end
+end
