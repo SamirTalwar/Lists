a50e46de5287bec04bcd3d8f2e2337a915ec2282
Florian Boehmak <florian.boehmak@googlemail.com>
2013-08-03 17:51:07 +0200

    it should return true for equal lists

---
+++ ruby/functional_list_spec.rb
@@ -0,0 +1,10 @@
+require_relative 'functional_list'
+
+describe "FunctionalList" do
+  it "returns true comparing two lists with the same values" do
+    list_one = FunctionalList.of(1, 2, 3)
+    list_two = FunctionalList.of(1, 2, 3)
+    list_one.should == list_two
+  end
+
+end
